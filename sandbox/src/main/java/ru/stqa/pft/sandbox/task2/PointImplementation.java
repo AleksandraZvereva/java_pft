383a4922e61991b37825199413dfafdee7a3b87d
AleksandraZvereva <102028402+AleksandraZvereva@users.noreply.github.com>
2022-04-22 13:36:03 +0300

    Реализация метода distance() вместо функции distance()

--- sandbox/src/main/java/ru/stqa/pft/sandbox/task2/PointImplementation.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/task2/PointImplementation.java
@@ -7,10 +7,9 @@ public class PointImplementation {
 
     Point p1 = new Point(2.6, 1.7);
     Point p2 = new Point(12.4, 3.6);
-    System.out.println(distance(p1, p2));
+    System.out.println(p1.distance(p2));
+    System.out.println(p2.distance(p1));
 
   }
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-  }
+
 }
