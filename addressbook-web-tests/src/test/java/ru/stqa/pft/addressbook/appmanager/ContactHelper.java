939f0c07e1bdd961f3d88382bff3eb8ba0040358
AleksandraZvereva <102028402+AleksandraZvereva@users.noreply.github.com>
2022-04-29 18:10:15 +0300

    Добавлен тестовый класс ContactModificationTests
    Добавлены методы initContactModification() и submitContactModification() во вспомогательный класс ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -20,6 +20,10 @@ public class ContactHelper extends HelperBase {
 
   public void acceptAlert(){wd.switchTo().alert().accept();}
 
+  public void initContactModification(){click(By.xpath("//*[@id=\"maintable\"]/tbody/tr[2]/td[8]/a/img"));}
+
+  public void submitContactModification(){click(By.xpath("//*[@id=\"content\"]/form[1]/input[22]"));}
+
   public void fillNewContactForm(ContactData contactData) {
     type(By.name("firstname"), contactData.getFirstName());
     type(By.name("lastname"), contactData.getLastName());
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactModificationTests extends TestBase{
+
+  @Test
+  public void testContactModification(){
+    app.getNavigationHelper().gotoHomePage();
+    app.getContactHelper().selectContact();
+    app.getContactHelper().initContactModification();
+    app.getContactHelper().fillNewContactForm(new ContactData("UpdateTest", "UpdateTestoff", "UpdateMoscow, Red Square, 1", "Updatetest_testoff@mail.ru", "Update123456789"));
+    app.getContactHelper().submitContactModification();
+    app.getNavigationHelper().gotoHomePage();
+  }
+}
