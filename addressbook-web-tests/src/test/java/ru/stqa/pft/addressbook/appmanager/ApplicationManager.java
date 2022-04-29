14d2631ecff4b33867a7a4c54a816834c7e142b1
AleksandraZvereva <102028402+AleksandraZvereva@users.noreply.github.com>
2022-04-29 15:16:03 +0300

    Создание двухуровневой архитектуры
    1. Создан класс TestBase - общий родитель для классов GroupCreationTests и GroupDeletionTests
    2. Создан класс ApplicationManager
    3. Между ними отношение делегирования: класс TestBase перед каждым тестовым методом инициализирует объект типа ApplicationManager,а после отработки метода - останавливает его, разрушает
    4. Распределены классы по пакетам tests, model, appmanager

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,16 +1,18 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-
-public class GroupCreationTests {
+public class ApplicationManager {
   private WebDriver wd;
 
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
+  public void init() {
     System.setProperty("chromedriver", "/usr/local/bin");
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -25,16 +27,6 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
-    gotoGroupPage();
-    initGroupCreation();
-    fillGroupForm(new GroupData("test1", "test2", "test3"));
-    submitGroupCreation();
-    returnToGroupPage();
-
   }
 
   public void returnToGroupPage() {
@@ -59,17 +51,16 @@ public class GroupCreationTests {
     wd.findElement(By.name("new")).click();
   }
 
-  private void gotoGroupPage() {
+  public void gotoGroupPage() {
     returnToGroupPage();
     wd.get("http://localhost/addressbook/group.php");
   }
 
-  @AfterMethod(alwaysRun = true)
-  public void tearDown() throws Exception {
+  public void stop() {
     wd.quit();
   }
 
-  private boolean isElementPresent(By by) {
+  public boolean isElementPresent(By by) {
     try {
       wd.findElement(by);
       return true;
@@ -78,7 +69,7 @@ public class GroupCreationTests {
     }
   }
 
-  private boolean isAlertPresent() {
+  public boolean isAlertPresent() {
     try {
       wd.switchTo().alert();
       return true;
@@ -87,5 +78,11 @@ public class GroupCreationTests {
     }
   }
 
+  public void deleteSelectedGroups() {
+    wd.findElement(By.name("delete")).click();
+  }
 
+  public void selectGroup() {
+    wd.findElement(By.name("selected[]")).click();
+  }
 }
